26613dd256eb57e8261b0bbe2db19b84275938e0
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-02-05 03:24:45 -0300

    Mejorando la estrella para que cada tanto se convierta en una taza de cafe!

--- sbfury/enemigos/estrella.py
+++ sbfury/enemigos/estrella.py
@@ -6,13 +6,14 @@
 
 import pilas
 import enemigo
+import random
 
 class Estrella(enemigo.Enemigo):
     """Una estrella ninja que vuela intentando golpear al shaolin."""
 
     def __init__(self, x, y, direccion, shaolin):
         enemigo.Enemigo.__init__(self)
-        self.imagen = "estrella.png"
+        self._cargar_imagen()
         self.shaolin = shaolin
         self.centro = ("centro", "centro")
         self.x = x
@@ -22,10 +23,18 @@ class Estrella(enemigo.Enemigo):
         self.actualizar()
         self.aprender(pilas.habilidades.Arrastrable)
 
+    def _cargar_imagen(self):
+        """Carga una imagen de estrella ninja o de una taza de cafe (!!!)."""
+        if random.randint(0, 10) < 8:
+            self.imagen = "estrella.png"
+        else:
+            self.imagen = "cafe.png"
+
     def actualizar(self):
         enemigo.Enemigo.actualizar(self)
         self.x += self.direccion * 6
         self.rotacion += 10
         self.z = self.y
+        self.sombra.escala = 0.5
 
         # TODO: eliminar si está fuera de la pantalla.
