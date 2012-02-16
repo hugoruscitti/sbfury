9db08d76511dfbba752a19a4e644fe12b805d96c
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-02-16 00:46:17 -0300

    creando las barras de energia.

--- sbfury/run.py
+++ sbfury/run.py
@@ -18,6 +18,9 @@ s = shaolin.Shaolin(lista_enemigos)
 lista_enemigos.append(enemigos.Red(s))
 escenario.Escenario(s)
 
+energia_shaolin = pilas.actores.Energia(x=-315, y=213, alto=20)
+energia_enemigo = pilas.actores.Energia(x=310, y=213, alto=20)
+
 #pilas.mundo.camara.x = [5300 - 640], 3
 #pilas.mundo.camara.x = [100], 40
 pilas.ejecutar()
