474ce8304c45e63214184cde50f2976724fd8455
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-02 01:15:09 -0300

    Mejorando los controles del shaolin para el movimiento de correr.

--- sbfury/shaolin/control.py
+++ sbfury/shaolin/control.py
@@ -11,7 +11,7 @@ class Control(pilas.control.Control):
 
     def __init__(self, escena):
         pilas.control.Control.__init__(self, escena)
-        self.historico = [' '] * 3
+        self.historico = [(0, ' ')] * 3
         self.corre = False
 
     def cuando_pulsa_una_tecla(self, evento):
@@ -21,10 +21,24 @@ class Control(pilas.control.Control):
 
     def cuando_suelta_una_tecla(self, evento):
         self.procesar_cambio_de_estado_en_la_tecla(evento.codigo, False)
+
         if not evento.es_repeticion:
             self.registrar_tecla(" ")
 
     def registrar_tecla(self, tecla):
-        self.historico.append(tecla)
+        self.historico.append((time.time(), tecla))
         self.historico.pop(0)
-        self.corre = (self.historico == [2, ' ', 2] or self.historico == [1, ' ', 1])
+
+        teclas_pulsadas = [tecla for (tiempo, tecla) in self.historico]
+        self.corre = False
+
+        # Si ha pulsado para comenzar a correr.
+        if teclas_pulsadas == [2, ' ', 2] or teclas_pulsadas == [1, ' ', 1]:
+            # Se asegura de que la pulsación ha sido rápida.
+            ultimo_time = self.historico[-1][0]
+            anteultimo_time = self.historico[-2][0]
+
+            dt = ultimo_time - anteultimo_time
+
+            if dt < 0.15:
+                self.corre = True
